744733fe19389efc72c509e031310501d9a4b746
root <jesusesteban12321@gmail.com>
2019-09-27 01:49:21 -0400

    (fix) agregar campo

--- app/MatterUser.php
+++ app/MatterUser.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class MatterUser extends Model
 {
      protected $table='matter_users';
-    protected $fillable = ['id','matter_id','user_id','type'];
+    protected $fillable = ['id','matter_id','user_id','type','admin_confirmed'];
    	public function user(){
     	return $this->belongsTo(User::class,'user_id');
     }
