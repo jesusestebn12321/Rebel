9556dc389f1bf695898c201a399cdd1a7fb8aa43
root <jesusesteban12321@gmail.com>
2019-09-27 00:44:10 -0400

    fix en first()

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -38,7 +38,7 @@ class UsersController extends Controller
     }
     public function show($slug){
         $career=Career::where('slug',$slug)->first();
-        $matter=Matter::where('career_id',$career->id)->get();
+        $matter=Matter::where('career_id',$career->id)->firstOrFail();
         $matter_user=MatterUser::all();
         return view('career.teacherMatterShow',compact('matter_user','matter','career'));
     }
