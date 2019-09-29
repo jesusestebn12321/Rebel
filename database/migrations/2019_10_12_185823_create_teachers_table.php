0ea9786394237fd2b64aafc2db315d2953e60bcd
root <jesusesteban12321@gmail.com>
2019-09-29 18:56:03 -0400

    migracion teacher

---
+++ database/migrations/2019_10_12_185823_create_teachers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug')->unique();
+            $table->unsignedInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')
+            ->on('users');
+
+            $table->integer('type')->default(0);
+            $table->boolean('admin_confirmed')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
