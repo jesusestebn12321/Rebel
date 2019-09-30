2f0dc78743cdf5d943a45a53880750a4c83f7bc2
root <jesusesteban12321@gmail.com>
2019-09-30 01:59:32 -0400

    fix en el boton de show de las tablas

--- resources/views/users/teacher/tables/tbodyVerify.blade.php
+++ resources/views/users/teacher/tables/tbodyVerify.blade.php
@@ -14,7 +14,7 @@
   <td>{{ $item->user->created_at }}</td>
   <td>{{ $item->user->updated_at }}</td>
   <td>
-    <a class="btn-primary btn" href="{{route('Profile.show',$item->user->slug)}}" title="Ver Usuario"><i class="fa fa-eye"></i></a>
+    <a class="btn-primary btn" href="{{route('Profile.Teacher.show',$item->slug)}}" title="Ver Usuario"><i class="fa fa-eye"></i></a>
     <a class="btn-danger btn" href="{{route('User.delete',$item->user->slug)}}" title="Borrar Usuario"><i class="fa fa-remove"></i></a>
     <a href="{{ route('admin.verify', $item->slug)}}" title="Remover Verificacion" class="btn-warning btn"><i class="fa fa-vimeo"></i></a>
  </td>
