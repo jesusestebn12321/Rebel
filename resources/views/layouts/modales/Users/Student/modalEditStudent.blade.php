866f08325b357574fe1d059e1b59f3e60d52a937
root <jesusesteban12321@gmail.com>
2019-09-30 12:58:12 -0400

    modal

---
+++ resources/views/layouts/modales/Users/Student/modalEditStudent.blade.php
@@ -0,0 +1,45 @@
+<div class="modal fade" id="editStudent">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="col">
+                    <h3><span class="fa fa-edit"></span> 
+                        Editar Estudiantes
+                    </h3>
+                </div>
+                <button class="close" aria-hidden="true" data-dismiss="modal" id='close'>&times;</button>
+            </div>
+            <div class="modal-body">
+                <form class="form-horizontal" method="PUT " action="{{route('Student.up_date',Auth::user()->slug)}}">
+                    
+                    {{ csrf_field() }}
+
+                    <input  type="hidden" id='ModalId' name="id">
+
+                    <div class="form-group">
+                        <label><i class="fa fa-tv"></i>Cedula</label>
+                        <input id="ModalDni" type="number" class="form-control" name="dni" autofocus required >
+                    </div>
+                    <div class="form-group">
+                        <label><i class="fa fa-tv"></i>Nombre</label>
+                        <input id="ModalName" type="text" class="form-control" name="name" autofocus required >
+                    </div>
+                    <div class="form-group">
+                        <label><i class="fa fa-tv"></i>Apellido</label>
+                        <input id="ModalLastname" type="text" class="form-control" name="lastname" autofocus required >
+                    </div>
+
+                </div>
+                <div class="modal-footer"> 
+                    <div class='container'>
+                        <div class="row">        
+                            <div class="col-12">
+                             <button href="#" class='btn btn-primary btn-block' id='btnEdit'>Editar</button>
+                         </div>
+                     </form>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </div>
+</div>
