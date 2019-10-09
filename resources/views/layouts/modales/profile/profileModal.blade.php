34367f96ffbac9d03463df7449a54f8277c05203
root <jesusesteban12321@gmail.com>
2019-10-09 17:10:22 -0400

    (fix) en la vista de profile

--- resources/views/layouts/modales/profile/profileModal.blade.php
+++ resources/views/layouts/modales/profile/profileModal.blade.php
@@ -4,43 +4,12 @@
 			<div class="modal-header">
 				<div class="col">
 					<h3><span class="fa fa-user"></span> 
-						Rellene el perfil de Profesor
+						Informacion
 					</h3>
 				</div>
 			</div>
 			<div class="modal-body">
-				<form class="form-horizontal" method="POST" action="{{route('MatterUser.store')}}">
-					{{ csrf_field() }}
-					<div class="form-group{{ $errors->has('areaOp') ? ' has-error' : '' }}">
-						<select id="areaOp"  class='form-control' value='0' name="areaOp" size='1'>       
-							<option value="0">Area</option>
-							@foreach($area as $item)
-							<option value="{{$item->id}}">{{$item->id}} - {{$item->area}}</option>
-							@endforeach                   
-						</select>
-					</div>
-					<div class="form-group{{ $errors->has('careerProfile') ? ' has-error' : '' }}">
-						<select id="careerProfile" class='form-control d-none' value='0' name="careerProfile" size='1'> 
-						<option value="0">Carrera</option>      
-						
-						</select>
-					</div>
-					<div class="form-group{{ $errors->has('matter_id') ? ' has-error' : '' }}">
-						<select id="matterOp" class='form-control d-none' value='0' name="matter_id" size='1'>       
-							<option value="0">Materia</option>                
-						</select>
-					</div>
-				</div>
-
-				<div class="modal-footer"> 
-					<div class='container'>
-						<div class="row">        
-							<div class="col-12">
-								<button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
-							</div>
-						</form>
-					</div>
-				</div>
+				<h2>No Tiene ningun tipo de unidad curricular asignada.</h2>	
 			</div>
 		</div>
 	</div>
