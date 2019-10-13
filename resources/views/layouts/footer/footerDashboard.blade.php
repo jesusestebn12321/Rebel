e2fa809094a11fb1aedeb9ab9d1bdf160cba5e17
root <jesusesteban12321@gmail.com>
2019-10-12 20:22:53 -0400

    fecha dinamica en los footers

--- resources/views/layouts/footer/footerDashboard.blade.php
+++ resources/views/layouts/footer/footerDashboard.blade.php
@@ -1,8 +1,6 @@
-<footer class="footer">
-  <div class="row align-items-center justify-content-xl-between">
     <div class="col-xl-6">
       <div class="copyright text-center text-xl-left text-muted">
-        &copy; 2019 <a href="#!" class="font-weight-bold ml-1" target="_blank"></a>
+        &copy; {{now("Y")}}<a href="#!" class="font-weight-bold ml-1" target="_blank"></a>
       </div>
     </div>
   </div>
--- resources/views/layouts/footer/footerLogin.blade.php
+++ resources/views/layouts/footer/footerLogin.blade.php
@@ -2,7 +2,7 @@
       <div class="row align-items-center justify-content-xl-between">
         <div class="col-xl-6">
           <div class="copyright text-center text-xl-left text-muted">
-            &copy; 2019
+            &copy; {{now("Y")}}
           </div>
         </div>
       </div>
