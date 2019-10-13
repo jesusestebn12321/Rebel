6035d0ad10ea672f50cc4c4ace7ac2bf90879e7b
root <jesusesteban12321@gmail.com>
2019-10-12 21:03:15 -0400

    fix en fecha dinamica

--- resources/views/layouts/footer/footerDashboard.blade.php
+++ resources/views/layouts/footer/footerDashboard.blade.php
@@ -1,6 +1,6 @@
     <div class="col-xl-6">
       <div class="copyright text-center text-xl-left text-muted">
-        &copy; {{now()->format('l jS \\of F Y h:i:s A')}}<a href="#!" class="font-weight-bold ml-1" target="_blank"></a>
+        &copy; {{now()->format('jS \\of F Y h:i:s A')}}<a href="#!" class="font-weight-bold ml-1" target="_blank"></a>
       </div>
     </div>
   </div>
--- resources/views/layouts/footer/footerLogin.blade.php
+++ resources/views/layouts/footer/footerLogin.blade.php
@@ -2,7 +2,7 @@
       <div class="row align-items-center justify-content-xl-between">
         <div class="col-xl-6">
           <div class="copyright text-center text-xl-left text-muted">
-            &copy; {{now()->format('l jS \\of F Y h:i:s A')}}
+            &copy; {{now()->format('jS \\of F Y h:i:s A')}}
           </div>
         </div>
       </div>
