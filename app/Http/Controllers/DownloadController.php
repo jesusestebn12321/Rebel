5adc854e0a2a5ece110df68a70d0db4727ea6e3e
root <jesusesteban12321@gmail.com>
2019-10-19 18:00:47 -0400

    metodos nuevos para el controlador DownloadController.php

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -6,79 +6,28 @@ use Illuminate\Http\Request;
 
 class DownloadController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+    //descargar pdf Equivalencias para los estudiantes 
+    public function equivalenciaStudents($slug){
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
     }
+    //descargar pdf de los profesores
+    public function adminTeacher(){
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
     }
+    //descargar pdf de las materias
+    public function adminMatters(){
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
     }
+    //descargar pdf careras 
+    public function adminCareers(){
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
+    //descargar pdf materias y contenido de la materias del profesor correspondiente
+    public function teacherMatter($slug){
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
     }
+    //descargar pdf de los profesores a quien coordina
+    public function CoordinadorTeachers($slug){
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
