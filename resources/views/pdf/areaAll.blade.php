39473ec354887a2b5ca3e6c5c5bc92137b823bca
root <jesusesteban12321@gmail.com>
2019-11-02 00:41:14 -0400

    feature para las vistas de los pdf

---
+++ resources/views/pdf/areaAll.blade.php
@@ -0,0 +1,1 @@
+@extends('pdf.layouts.app')
---
+++ resources/views/pdf/layouts/app.blade.php
@@ -0,0 +1,37 @@
+
+
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+        <style>
+        h1{
+        text-align: center;
+        text-transform: uppercase;
+        }
+        .contenido{
+        font-size: 20px;
+        }
+        #primero{
+        background-color: #ccc;
+        }
+        #segundo{
+        color:#44a359;
+        }
+        #tercero{
+        text-decoration:line-through;
+        }
+    </style>
+    </head>
+    <body>
+        <h1>Titulo de prueba</h1>
+        <hr>
+        <div class="contenido">
+            <p id="primero">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore nihil illo odit aperiam alias rem voluptatem odio maiores doloribus facere recusandae suscipit animi quod voluptatibus, laudantium obcaecati quisquam minus modi.</p>
+            <p id="segundo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore nihil illo odit aperiam alias rem voluptatem odio maiores doloribus facere recusandae suscipit animi quod voluptatibus, laudantium obcaecati quisquam minus modi.</p>
+            <p id="tercero">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore nihil illo odit aperiam alias rem voluptatem odio maiores doloribus facere recusandae suscipit animi quod voluptatibus, laudantium obcaecati quisquam minus modi.</p>
+        </div>
+    </body>
+</html>
+
