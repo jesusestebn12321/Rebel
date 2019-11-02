5a73177a84f903ea2aaf7d71b652d5fe5d0e5ecb
root <jesusesteban12321@gmail.com>
2019-11-02 00:37:12 -0400

    feature de metodo de reporte de las areas

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -3,7 +3,10 @@
 namespace Equivalencias\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Equivalencias\Career;
+use Equivalencias\Area;
+use PDF;
+use Carbon\Carbon;
 class DownloadController extends Controller
 {
     //descargar pdf Equivalencias para los estudiantes 
@@ -19,8 +22,19 @@ class DownloadController extends Controller
 
     }
     //descargar pdf careras 
+    public function adminArea(){
+        $area=Area::all();
+        $today = Carbon::now()->format('l jS \\of F Y h:i:s A');
+        
+        $pdf=PDF::loadView('pdf.areaAll',compact('area','today'));
+        $pdf->download('ReportesDeLasAreas.pdf');
+        
+        return $pdf->stream();
+    }
     public function adminCareers(){
-
+        $career=Career::all();
+        $pdf=PDF::loadView('pdf.layouts.app');
+        return $pdf->download('tuto.pdf');
     }
     //descargar pdf materias y contenido de la materias del profesor correspondiente
     public function teacherMatter($slug){
