1e12d368f45331d956373d62377e0a8f49143ae2
root <jesusesteban12321@gmail.com>
2019-11-05 23:06:12 -0400

    relacion y metodo de validacion de rol

--- app/Teacher.php
+++ app/Teacher.php
@@ -3,16 +3,36 @@
 namespace Equivalencias;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Auth;
 class Teacher extends Model
 {
     protected $table='teachers';
-    protected $fillable = ['id','slug','user_id','type','admin_confirmed'];
+    protected $fillable = ['id','slug','user_id','rol_id','admin_confirmed'];
     public function user(){
     	return $this->belongsTo(User::class,'user_id');
     }
     public function matter(){
     	return $this->hasMany(MatterUser::class,'user_id');
+    }
+    private function checkIfUserHasRole($need_role){
+        return (strtolower($need_role) == strtolower($this->rol_id)) ? true : null;
+    }
+    
+    public function hasRole($rol){
+        if (is_array($rol)) {
+            foreach ($rol as $need_role){
+                if ($this->checkIfUserHasRole($need_role)){
+                    return true;
+                }
+            }
+        }else {
+            return $this->checkIfUserHasRole($rol);
+        }
+        return false;
+    }
+    public function rol(){
+        return $this->hasMany(Rol::class,'rol_id');
     }
     
     
