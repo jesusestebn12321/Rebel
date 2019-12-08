d302504575fabf4e3df186337c1dc00757511a94
root <jesusesteban12321@gmail.com>
2019-12-07 22:57:28 -0400

    fix en el metodo verify, nuevo metodo one_content este metodo es para que si de por casualidad hay dos contenidos vigentes, pueda seleccionar el contenido que quiere como vigente para la materia

--- app/Http/Controllers/adminCurricular.php
+++ app/Http/Controllers/adminCurricular.php
@@ -4,17 +4,40 @@ namespace Equivalencias\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Equivalencias\Content;
-
+use Equivalencias\Matter;
+use input;
 class adminCurricular extends Controller
 {
     
     public function verify($slug){
-        //
         $content=Content::where('slug',$slug)->first();
+       
+        $content_verify=Content::where('matter_id',$content->matters->id)->get();
+
+        foreach ($content_verify as $item) {
+            # code...
+            $item->confirmation=false;
+            $item->status=0;
+            $item->save();
+        }
         $content->confirmation=true;
         $content->status=1;
         $content->save();
+
         return back()->with('success','Contenido Verificado.');
+    }
+    public function one_content($id,Request $request){
+        $matter=Matter::where('id',$id)->first();
+        foreach ($request->input() as $item) {
+            foreach ($matter->content as $items) {
+                if ($item == $items->id) {
+                    $items->status=0;
+                    $items->confirmation=false;
+                    $items->save();
+                }
+            }
+        }    
+        return redirect()->route('Matters.show',$matter->slug);
     }
 
     public function remove($slug){
