a2eba9ca9266b49cb32ca954d0acb3308466dbd7
root <jesusesteban12321@gmail.com>
2019-12-15 22:24:57 -0400

    feature en el metodo de QRContent para ver los contenidos que viste en e trascurso de tu inicio estudiantil

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -3,28 +3,42 @@
 namespace Equivalencias\Http\Controllers;
 
 use SimpleSoftwareIO\QrCode\BaconQrCodeGenerator;
+use Equivalencias\MatterUser;
+use Equivalencias\University;
 use Illuminate\Http\Request;
+use Equivalencias\Download;
 use Equivalencias\Career;
 use Equivalencias\Area;
-use PDF;
 use Carbon\Carbon;
-use Auth;
-use url;
 use QrCode;
+use Auth;
+use PDF;
+use url;
 class DownloadController extends Controller
 {
     //descargar pdf Equivalencias para los estudiantes 
     public function equivalenciaStudents($slug){
 
     }
+    
     //descargar pdf de los profesores
     public function adminTeacher(){
-
+        $teacher=Teacher::all();
+        $today = Carbon::now()->format('l jS \\of F Y h:i:s A');
+        $pdf=PDF::loadView('pdf.areaAll',compact('teacher','today'));
+        $pdf->download('Reportes_De_Los_Profesores_y_Las_Profesoras.pdf');
+        return $pdf->stream();
     }
+    
     //descargar pdf de las materias
     public function adminMatters(){
-
+        $matter=Matter::all();
+        $today = Carbon::now()->format('l jS \\of F Y h:i:s A');
+        $pdf=PDF::loadView('pdf.areaAll',compact('matter','today'));
+        $pdf->download('Reportes_De_Las_Materias.pdf');
+        return $pdf->stream();   
     }
+    
     //descargar pdf careras 
     public function adminArea(){
         $area=Area::all();
@@ -33,19 +47,36 @@ class DownloadController extends Controller
         $pdf->download('ReportesDeLasAreas.pdf');
         return $pdf->stream();
     }
-    public function QRContents(){
 
-     
-        
+    public function QRContents(Request $request){ 
+        $today=Carbon::now()->format('l jS \\of F Y h:i:s A');
+        $slug=str_slug(Auth::user()->id.'_'.$today.'_'.rand());
+        $download=Download::create([
+            'slug'=>$slug,
+            'user_id'=>Auth::user()->id,
+            'start_student'=>$request->start_student,
+            'last_student'=>$request->last_student,
+            'status'=>true
+            ]);
         $area=Area::all();
-        $today = Carbon::now()->format('l jS \\of F Y h:i:s A');
-        $urlConfir=url('').'/CodigoQEverificarPDF/'. Auth::user()->id;
-        return View('QR.pdfContentQR',compact('area','today','urlConfir'));
-        //$pdf->download('Reporte_De_Los_Contenidos_'.Auth::user()->dni.'.pdf');
+        $university=University::find(1);
+        $matter_user=MatterUser::where('user_id',Auth::user()->id)->get();
+        
+        $urlConfir=url('').'/CodigoQEverificarPDF/'.$download->slug;
+        
+        //$pdf=PDF::loadView('QR.pdfContentQR',compact('university','matter_user','today','urlConfir','download','area'));
+        //$matter
+
+
+
+        return View('QR.pdfContentQR',compact('university','matter_user','today','urlConfir','download','area'));
+
+        $pdf->download('Reporte_De_Los_Contenidos_'.Auth::user()->dni.'.pdf');
+        
         return $pdf->stream();
-
-
     }
+
+    //descargar pdf careras 
     public function adminCareers(){
         $career=Career::all();
         $pdf=PDF::loadView('pdf.layouts.app');
