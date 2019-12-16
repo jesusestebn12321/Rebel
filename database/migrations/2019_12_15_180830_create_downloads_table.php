664a83b307200e67cdd00d75fbd32190e774df2b
root <jesusesteban12321@gmail.com>
2019-12-15 22:23:25 -0400

    feature migracion downloand

---
+++ database/migrations/2019_12_15_180830_create_downloads_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDownloadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('downloads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug')->unique();
+            $table->unsignedInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')
+                  ->on('users')
+                  ->onDelete('set null');
+            
+            $table->string('start_student')->nullable();
+            $table->string('last_student')->nullable();
+            $table->boolean('status');
+            $table->timestamps();
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('downloads');
+    }
+}
