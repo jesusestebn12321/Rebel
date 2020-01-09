bc64714bded7df9cd76facc6be02000c5faf8966
root <jesusesteban12321@gmail.com>
2020-01-09 12:01:56 -0400

    feature en vista de resgitro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,11 +39,12 @@
                 <small>Datos personales para crear un usuario</small>
               </div>
               <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
                 
-                <div class="form-group">
-                    <div class="container-fluid">
-                  <div class="row">
+                {{ csrf_field() }}
+                
+                <div class="form-group{{ $errors->has('rol') ? ' has-error' : '' }}">
+                  <div class="container-fluid">
+                    <div class="row">
                       <div class="col-6">
                         <div class="input radios">
                           <input type="radio" name="rol" id="students" value="students"/>
@@ -57,88 +58,86 @@
                         </div>
                       </div>
                     </div>
+                    @if ($errors->has('rol'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('rol') }}</strong>
+                    </span>
+                  @endif
                   </div>
                 </div>
+
                 <hr>
+                
                 <div class="form-group{{ $errors->has('dni') ? ' has-error' : '' }}">
                   <div class="input-group input-group-alternative mb-3">
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="ni ni-credit-card"></i></span>
                     </div>
                     <input id="dni" type="number" placeholder="Cedula" class="form-control" name="dni" value="{{ old('dni') }}" required autofocus>
-                    @if ($errors->has('dni'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('dni') }}</strong>
-                        </span>
-                    @endif
                   </div>
+                  @if ($errors->has('dni'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('dni') }}</strong>
+                    </span>
+                  @endif
                 </div>
+
                 <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                   <div class="input-group input-group-alternative mb-3">
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="ni ni-bold-right"></i></span>
                     </div>
                     <input id="name" type="text" placeholder="Nombre" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-                    @if ($errors->has('name'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('name') }}</strong>
-                        </span>
-                    @endif
                   </div>
+                  @if ($errors->has('name'))
+                      <span class="help-block">
+                          <strong>{{ $errors->first('name') }}</strong>
+                      </span>
+                  @endif
                 </div>
+
                 <div class="form-group{{ $errors->has('lastname') ? ' has-error' : '' }}">
                   <div class="input-group input-group-alternative mb-3">
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="ni ni-bold-right"></i></span>
                     </div>
                     <input id="lastname" type="text" placeholder="Apellido" class="form-control" name="lastname" value="{{ old('lastname') }}" required autofocus>
-                    @if ($errors->has('lastname'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('lastname') }}</strong>
-                        </span>
-                    @endif
                   </div>
+                  @if ($errors->has('lastname'))
+                      <span class="help-block">
+                          <strong>{{ $errors->first('lastname') }}</strong>
+                      </span>
+                  @endif
                 </div>
-{{-- en produccion --}}
-               {{--  <div class="form-group">
-                    <select id="career_id" class='form-control' value='0' name="career_id" size='1'>
-                      <option value="0">Carreras</option>
-                      @foreach($career as $item)
-                        <option value="{{$item->id}}">{{$item->career}}</optio>
-                      @endforeach                   
-                    </select>
-                </div>
-                <div class="form-group">
-                   <select id="area_id" class='form-control d-none' value='0' name="area_id" size='1'></select>
-                </div> --}}
-                
-                {{--  --}}
+
                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                   <div class="input-group input-group-alternative mb-3">
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="ni ni-email-83"></i></span>
                     </div>
                     <input id="email" type="email" placeholder="E-mail"  class="form-control" name="email" value="{{ old('email') }}" required>
-                      @if ($errors->has('email'))
-                          <span class="help-block">
-                              <strong>{{ $errors->first('email') }}</strong>
-                          </span>
-                      @endif
                   </div>
+                  @if ($errors->has('email'))
+                    <span class="help-block">
+                      <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                  @endif
                 </div>
+
                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                   <div class="input-group input-group-alternative">
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
                     </div>
                     <input id="password" placeholder="Contraseña" type="password" class="form-control" name="password" required>
-                    @if ($errors->has('password'))
-                      <span class="help-block">
-                        <strong>{{ $errors->first('password') }}</strong>
-                      </span>
-                    @endif
                   </div>
+                  @if ($errors->has('password'))
+                    <span class="help-block">
+                      <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                  @endif
                 </div>
+
                 <div class="form-group">
                   <div class="input-group input-group-alternative">
                     <div class="input-group-prepend">
@@ -148,7 +147,7 @@
                   </div>
                 </div>
                 <div class="text-center">
-                  <button type="submit" class="btn btn-primary mt-4">Create account</button>
+                  <button type="submit" class="btn btn-primary mt-4">Crear cuenta</button>
                 </div>
               </form>
             </div>
