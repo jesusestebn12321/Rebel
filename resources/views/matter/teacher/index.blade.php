cdd6782a01dddee1ad1bb7cef371c21b69066b13
root <jesusesteban12321@gmail.com>
2020-01-15 21:46:55 -0400

    se colocaron los contenidos en tabla para que se visualice mejor

--- resources/views/matter/teacher/index.blade.php
+++ resources/views/matter/teacher/index.blade.php
@@ -111,121 +111,72 @@
             </div>
           </div>
         </div>
-
-        @forelse($matter_user->matter->content as $item)
-        <div class="col-xl-6 order-xl-2 mb-5 mb-xl-0 pt-8 pt-md-4 pb-0 pb-md-4">
-          <div class="card card-profile shadow">
-            <div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
-              <div class="d-flex justify-content-between">
-                @if($teacher->hasRole(5))
-                <a href="{{route('Contents.delete',$item->slug)}}" class="btn btn-sm btn-danger mr-4">borrar</a>
-                <a href="#" id="edit1_{{$item->id}}" onclick="edit1({{$item->id}})" class="btn btn-sm btn-default float-right">Editar</a>
-                @endif
-                @if($item->status==false)
-                <span class="float-right badge-pill badge badge-danger">Verción: {{$item->version}} - {{$item->status==true?'actualizada':'Desactualizada'}}</span>
-                @else
-                <span class="float-right badge-pill badge badge-success">Verción: {{$item->version}} - {{$item->status==true?'Actualizada':'Desactualizada'}}</span>
-                @endif
-                @if($item->confirmation==false)
-                <span class="float-right badge-pill badge badge-danger">{{$item->confirmation==true?'confimada':'espera por la confirmación....'}}</span>
-                @else
-                <span class="float-right badge-pill badge badge-success">{{$item->confirmation==true?'confimada':'espera por la confirmación....'}}</span>
-                @endif
-              </div>
-                <br>
-                @if($teacher->hasRole(5))
-              <div class="d-flex justify-content-between">
-                <form method="POST" action="{{route('rollback')}}">
-                    {{ csrf_field() }}
-                    <input type="hidden" name="content_id" value="{{$item->id}}">
-                    <div class="container">
-                      <div class="row">
-                        <div class="col-8">
-                          <select class='form-control col' value='0' name="slug" size='1'>
-                              <option class='text-center' value="0">------ Versiones ------</option>
-                              @forelse($item->contentVersion as $items)
-
-                                <option value="{{$items->slug}}">Version | {{$items->version}} - {!!$items->title!!}</option>
-                              @empty
-                                <option><label>------ No hay versiones para esta materia ------</label></option>
-                              @endforelse                   
-                          </select>
-                          
-                        </div>
-                        <div class="col-4">
-                          <button type="submit" class="btn btn-block btn-info float-right">rollback</button>
-                          
-                        </div>
-                      </div>
-                    </div>
-                  
-
-                </form>
-              </div>
-                @endif
-              
-            </div>
-            <div class="card-body pt-0 pt-md-4">
-              <div class="text-center">
-                <h3 class="text-center">
-                <form action="{{route('Contents.up_date',$item->slug)}}">
-                <input type="hidden" id="id_matter" value="{{$matter_user->matter->id}}" >
-                  <blockquote>{!!$item->title!!}</blockquote>
-                  <textarea id="title{{$item->id}}" class="d-none form-control" name="title">{!!$item->title!!}</textarea>
-                </h3>
-                <hr class="my-4">
-                <label>Introduccion:</label>
-                <blockquote>{!! $item->introdution !!}</blockquote>
-                <textarea name="introdution" id="introdution{{$item->id}}" class="d-none form-control" >{!! $item->introdution !!}</textarea>
-
-                <hr class="my-4">
-                <label>Contenido:</label>
-                <blockquote>{!! $item->content !!}</blockquote>
-                <textarea name="content" id="content{{$item->id}}" class="d-none form-control" >{!! $item->content !!}</textarea>
-
-
-              </div>
-            </div>
-
-            <div class="card-footer">
-              <div class="text-center">
-                  <div class="row">
-                    <div class="col-6">
-                      <h4>Creada</h4>
-                      <div class="h5 mt-4">
-                        <i class="ni business_briefcase-24 mr-2"></i>{{$item->created_at}} 
-                      </div>
-                    </div>
-                    <div class="col-6">                        
-                      <h4>Editada</h4>
-                      <div class="h5 mt-4">
-                        <i class="ni business_briefcase-24 mr-2"></i>{{$item->updated_at}} 
-                      </div>
-                    </div>
-                  </div>
-                <button type="submit" onclick="edit2({{$item->id}})" id="edit2_{{$item->id}}" class="d-none btn btn-block btn-info">Editar</button>
-                </form>
-              </div>
-            </div>
-
-
-
-
-          </div>
-        </div>
-    @empty
-      <div class="col-xl-12 order-xl-2 mb-5 mb-xl-0 pt-8 pt-md-4 pb-0 pb-md-4">
-          <div class="card bg-info card-profile shadow">
-            <div class="card-body pt-0 pt-md-4">
-              <div class="text-center">
-                <h3 class="text-white">
-                  No hay contenido para esta unidad curricular
-                </h3>
-              </div>
-            </div>
-          </div>
-        </div>
-    @endforelse
       </div>
-    </div>
+      <div class="row mt-5">
+        <div class="col">
+          <div class="card bg-default shadow">
+            <div class="card-header bg-transparent border-0">
+              <div class="row align-items-center">
+                <div class="col-8">
+                  <h3 class="text-white mb-0">Contenidos</h3>
+                </div>
+              </div>
+            </div>
+            <div class="table-responsive">
+              <table class="table align-items-center table-dark table-flush">
+                <thead class="thead-dark">
+                  <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Codigo</th>
+                    <th scope="col">Titulo</th>
+                    <th scope="col">Version</th>
+                    <th scope="col">Estatus</th>
+                    <th scope="col">Confirmacón</th>
+                    <th scope="col">Creado</th>
+                    <th scope="col">Editado</th>
+                    <th scope="col"></th>
+                  </tr>
+                </thead>
+                @forelse($matter_user->matter->content as $item)
+                  <tbody><tr></tr>
+                    <tr>
+                      <td>{{$item->id}}</td>
+                      <td>{!!$item->slug!!}</td>
+                      <td><label>{!!$item->title!!}</label></td>
+                      <td>{!!$item->version!!}</td>
+                      <td>
+                        @if($item->status==false)
+                        {{$item->status==true?'actualizada':'Desactualizada'}}
+                        @else
+                        {{$item->status==true?'Actualizada':'Desactualizada'}}
+                        @endif
+                      </td>
+                      <td>
+                        @if($item->confirmation==false)
+                         {{$item->confirmation==true?'confimada':'espera por la confirmación....'}}
+                        @else
+                        {{$item->confirmation==true?'confimada':'espera por la confirmación....'}}
+                        @endif
+                      </td>
+                      <td id='td_Create{{$item->id}}'>{{ $item->created_at }}</td>
+                      <td id='td_Edit{{$item->id}}'>{{ $item->updated_at }}</td>
+                      <td>
+                        @if($teacher->hasRole(5))
+                        <a href="{{route('Contents.delete',$item->slug)}}" class="btn btn-sm btn-danger">Borrar</a>
+
+                        <a href="{{route('Contents.edit',$item->slug)}}" id="edit1_{{$item->id}}" class="btn btn-sm btn-info">Editar</a>
+
+                        <a href="{{route('Contents.show', $item->slug)}}" class="btn btn-sm btn-warning">Ver</a>
+                        @endif   
+                      </td>
+                    </tr>
+                  </tbody>
+                @empty
+                
+                @endforelse
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
 @endsection
