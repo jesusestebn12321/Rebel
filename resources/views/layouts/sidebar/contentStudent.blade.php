306b952bbc4c0352f10de7663da656a76d5dcc43
root <jesusesteban12321@gmail.com>
2020-01-17 03:15:53 -0400

    route para descargar equivalencias

--- resources/views/layouts/sidebar/contentStudent.blade.php
+++ resources/views/layouts/sidebar/contentStudent.blade.php
@@ -1,5 +1,5 @@
 <li class="nav-item">
-  <a class="nav-link font-italic" data-target='#downloadEquivalencias' data-toggle='modal' href="#">
+  <a class="nav-link font-italic" href="{{route('equivalencia',Auth::user()->id)}}">
     <i class="ni ni-cloud-download-95 text-info"></i> Descargar Eqivalencia
   </a>
 </li>
