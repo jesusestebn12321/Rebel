b3d83e9b1f41d118bebc5956ab8f39f9e8479cbb
root <jesusesteban12321@gmail.com>
2020-01-17 03:11:28 -0400

    modelo y migacion para las descargas

---
+++ app/Download.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Equivalencias;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Download extends Model
+{
+    protected $table='downloads';
+    protected $fillable = ['id','slug','user_id','last_student','start_student', 'status'];
+
+    public function user(){
+    	return $this->belongsTo(User::class,'user_id');
+    }
+
+}
