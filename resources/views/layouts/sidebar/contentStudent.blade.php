2864e370b675da005035255aae98029787315dad
root <jesusesteban12321@gmail.com>
2020-01-17 03:21:27 -0400

    target="_blank"

--- resources/views/layouts/sidebar/contentStudent.blade.php
+++ resources/views/layouts/sidebar/contentStudent.blade.php
@@ -1,5 +1,5 @@
 <li class="nav-item">
-  <a class="nav-link font-italic" href="{{route('equivalencia',Auth::user()->id)}}">
+  <a class="nav-link font-italic" target="_blank" href="{{route('equivalencia',Auth::user()->id)}}">
     <i class="ni ni-cloud-download-95 text-info"></i> Descargar Eqivalencia
   </a>
-</li>
+</li> 
