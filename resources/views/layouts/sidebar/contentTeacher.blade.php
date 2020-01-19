243e2edcd98d2a7f2d03843f26d4f82cc7b58ed7
root <jesusesteban12321@gmail.com>
2020-01-19 04:06:21 -0400

    ahora no hay que mandar variables a la vista de teacher

--- resources/views/layouts/sidebar/contentTeacher.blade.php
+++ resources/views/layouts/sidebar/contentTeacher.blade.php
@@ -3,13 +3,10 @@
 		<i class="ni ni-book-bookmark text-indigo"></i> Unidad Curricular
 	</a>
 </li>
-@if($teacher)
-	@if($teacher->hasRole(5))
+@if(Auth::user()->teacher[0]->hasRole(5))
 	<li class="nav-item">
 		<a class="nav-link font-italic" href="{{route('Matter.asignar.index')}}">
 			<i class="ni ni-folder-17 text-info"></i> Asignar materia al profesor
 		</a>
 	</li>
-	@else
-	@endif
 @endif
