5d6c5203aa57bd977a88b9d8d2b5faa102071eb8
root <jesusesteban12321@gmail.com>
2020-01-21 20:26:55 -0400

    se colocan titulos en español

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -67,25 +67,23 @@
 @endif
 @if(!Auth::user()->hasRole(3))  
 <div class="row">
-
 	<div class="col-xl-12">
-          <div class="card shadow">
-            <div class="card-header bg-transparent">
-              <div class="row align-items-center">
-                <div class="col">
-                  <h6 class="text-uppercase text-muted ls-1 mb-1">Estadisticas</h6>
-                </div>
-              </div>
-            </div>
-            <div class="card-body">
-              <!-- Chart -->
-              <div class="chart">
-                <canvas id="chart" class="chart-canvas"></canvas>
-              </div>
-            </div>
-          </div>
-        </div>
-	
+	  <div class="card shadow">
+	    <div class="card-header bg-transparent">
+	      <div class="row align-items-center">
+	        <div class="col">
+	          <h6 class="text-uppercase text-muted ls-1 mb-1">Estadisticas</h6>
+	        </div>
+	      </div>
+	    </div>
+	    <div class="card-body">
+	      <!-- Chart -->
+	      <div class="chart">
+	        <canvas id="chart" class="chart-canvas"></canvas>
+	      </div>
+	    </div>
+	  </div>
+	</div>
 </div>
 @endif
 @endsection
