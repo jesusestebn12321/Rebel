eec4ef220cd009a8bd02806422aa6e3b2796efd1
root <jesusesteban12321@gmail.com>
2020-01-21 20:22:40 -0400

    departamento migracion

---
+++ database/migrations/2019_07_05_023810_create_departments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('slug')->unique();
+            $table->string('department');
+            
+            $table->unsignedInteger('area_id')->nullable();
+            $table->foreign('area_id')->references('id')
+                    ->on('areas')
+                    ->onDelete('cascade');
+           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departments');
+    }
+}
