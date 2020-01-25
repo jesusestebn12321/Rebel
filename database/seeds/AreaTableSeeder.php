3cc9d2123b4983c5502ccf090dc99eed3645a273
root <jesusesteban12321@gmail.com>
2020-01-25 05:33:53 -0400

    seed new

--- database/seeds/AreaTableSeeder.php
+++ database/seeds/AreaTableSeeder.php
@@ -13,14 +13,179 @@ class AreaTableSeeder extends Seeder
     {
         //
         Area::create([
-            'slug'=> str_slug('Infomatica-Sanjuan_1'),
+            'slug'=> str_slug('nfomati32432ca-Sanjuan_1'),
             'area'=>'Informatica',
             'address_id'=>1,
             'university_id'=>1,
         ]);
         Area::create([
-            'slug'=> str_slug('Medicina-Sanjuan_1'),
-            'area'=>'Medicina',
+            'slug'=> str_slug('edicin2234a-Sanjuan_2'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_3'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_4'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_5'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_6'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_7'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_8'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_9'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_10'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_11'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_12'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_13'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_14'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_1111'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_111'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_1111111'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_22222'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edic345345ina-Sanjuan_'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicin5435345a-Sanjuan_'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edic345435ina-Sanjuan_'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-San345juan_'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-S3445345anjuan_'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanj345345uan_'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-San435345juan_'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sa123njuan_'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sa123213njuan_'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-S123123anjuan_'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-S3123anjuan_'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-123123Sanjuan_'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina123123-Sanjuan_'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_12321333'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_123123'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_123'),
+            'area'=>'edicina',
+            'address_id'=>1,
+            'university_id'=>1,
+        ]);Area::create([
+            'slug'=> str_slug('edicina-Sanjuan_3333'),
+            'area'=>'edicina',
             'address_id'=>1,
             'university_id'=>1,
         ]);
