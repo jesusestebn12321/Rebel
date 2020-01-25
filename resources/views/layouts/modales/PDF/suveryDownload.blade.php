009b7c26c4135027ae4fc41c2f6b76ddd9566fe8
root <jesusesteban12321@gmail.com>
2020-01-25 01:00:55 -0400

    modal para descargar pdfs

--- resources/views/layouts/modales/PDF/suveryDownload.blade.php
+++ resources/views/layouts/modales/PDF/suveryDownload.blade.php
@@ -4,40 +4,32 @@
             <div class="modal-header">
                 <div class="col">
                     <h3><span class="fa fa-user"></span> 
-                    Descargar contenidos
+                    Descargar PDF
                     </h3>
                 </div>
-                    <button class="close" aria-hidden="true" data-dismiss="modal" id='close'>&times;</button>
-                
+                <button class="close" aria-hidden="true" data-dismiss="modal" id='close'>&times;</button>
             </div>
             <div class="modal-body">
-                <form class="form-horizontal" method="GET" action="{{route('equivalencia',Auth::user()->id)}}">
+                <form class="form-horizontal" method="POST" action="{{route('equivalencia',Auth::user()->slug)}}">
                     {{ csrf_field() }}
+                    
                     <div class="form-group">
-                      <label>Inicio Estudiantil:</label>
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fa fa-calendar"></i></span>
-                            </div>
-                            <input type="text" name='start_student' class="form-control" data-inputmask="'alias': 'dd/mm/yyyy'" data-mask>
-                        </div>
-                    </div>
-                
-                    <div class="form-group">
-                        <label>Culminación Estudiantil:</label>
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="fa fa-calendar"></i></span>
-                            </div>
-                            <input type="text" name="last_student" class="form-control" data-inputmask="'alias': 'yyy/mm/dd'" data-mask>
-                        </div>
+                      <div class="col-md-6 offset-md-4">
+                          <div class="g-recaptcha" data-sitekey="{{ env('CAPTCHA_SITE_KEY') }}"></div>
+                          @if ($errors->has('g-recaptcha-response'))
+                              <span class="invalid-feedback" style="display: block;">
+                                  <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
+                              </span>
+                          @endif
+                      </div>
+
                     </div>
                 </div>
                 <div class="modal-footer"> 
                     <div class='container'>
                         <div class="row">        
                             <div class="col-12">
-                                <button type="submit" class="btn btn-primary btn-block btn-flat">Register</button>
+                                <button target="_blank" type="submit" class="btn btn-primary btn-block btn-flat">Descargar</button>
                             </div>
                        </form>
                    </div>
