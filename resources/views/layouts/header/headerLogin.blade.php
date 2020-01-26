5ac611132bfdd80698e08f7e6ac6c3cdf6afe3d4
root <jesusesteban12321@gmail.com>
2020-01-26 01:02:08 -0400

    se agrega el boton para poder deslegar el modal para descargar con tenidos publicos

--- resources/views/layouts/header/headerLogin.blade.php
+++ resources/views/layouts/header/headerLogin.blade.php
@@ -1,15 +1,19 @@
  <div class="container">
-        <div class="header-body text-center mb-7">
-          <div class="row justify-content-center">
-            <div class="col-lg-5 col-md-6">
-              <h1 ><a href="{{url('/')}}" class="text-white">Bienvenido/a!</a></h1>
-              <p class="text-lead text-light">Sistema de equivalencias Rebel </p>
-            </div>
-          </div>
-        </div>
+  <div class="header-body text-center mb-7">
+    <div class="row justify-content-center">
+      <div class="col-lg-12 col-md-12">
+        <h1 ><a href="{{url('/')}}" class="text-white">Bienvenido/a!</a></h1>
+        <p class="text-lead text-light">Sistema de equivalencias Rebel </p>
       </div>
-      <div class="separator separator-bottom separator-skew zindex-100">
-        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1">
-          <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
-        </svg>
+      <div class="col-lg-5 col-md-6">
+        <h1 ><a href="#" class="text-white">Descarga de las materias</a></h1>
+        <p class="text-lead text-light"><a href="#" data-target='#downloadPublic' data-toggle='modal' class="btn bg-gradient-indigo text-white" >Descargar</a></p>
       </div>
+    </div>
+  </div>
+</div>
+<div class="separator separator-bottom separator-skew zindex-100">
+  <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1">
+    <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+  </svg>
+</div>
