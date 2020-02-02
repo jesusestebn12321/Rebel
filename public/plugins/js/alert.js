0b5d1295d0b0da456bd9f01f04f3cc94f9071259
root <jesusesteban12321@gmail.com>
2020-02-02 00:55:14 -0400

    feature de alert swal

---
+++ public/plugins/js/alert.js
@@ -0,0 +1,73 @@
+function deletes(slug,url){
+    swal({
+      title: "¿Desea eliminar este item "+slug+"?",
+      text: "El item se eliminara en caso de que precione OK!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        $.ajax({
+          url: APP_URL+url+slug,
+          method: 'GET',
+          success: function (respuesta) {
+            swal({
+              icon: "success",
+              title: "Exito",
+              text: "Sea eliminado el item con exito!",
+            });
+            location.reload();
+
+          },error(e){
+            swal('Error en url',{
+              icon: "error",
+            });
+          }
+        });
+      }else{
+        swal("Se cancelo la acción!",{
+          title:'Acción Cancelada',
+          icon:"info",
+        });
+      }
+    });
+  }
+
+function verify_user(slug,url,string){
+    swal({
+      title: "¿Desea "+ string +" al usuario "+slug+"?",
+      text: "El usuario se le "+ string +" en caso de que precione OK!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        $.ajax({
+          url: APP_URL+url+slug,
+          method: 'GET',
+          success: function (respuesta) {
+            swal({
+              icon: "success",
+              title: "Exito",
+              text: "Sea le "+ string +" con exito!",
+            });
+            location.reload();
+
+          },error(e){
+            swal('Error en url',{
+              icon: "error",
+            });
+          }
+        });
+      }else{
+        swal("Se cancelo la acción!",{
+          title:'Acción Cancelada',
+          icon:"info",
+        });
+      }
+    });
+  }
+
+
+
+
