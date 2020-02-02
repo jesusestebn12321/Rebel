161bf94db04f5fdeafe0bec5d641ad4187aee3db
root <jesusesteban12321@gmail.com>
2020-02-02 01:01:07 -0400

    se cambia el for por un forelse

--- resources/views/career/showTeacherMatterUser.blade.php
+++ resources/views/career/showTeacherMatterUser.blade.php
@@ -12,75 +12,72 @@
       success: function (response) {
         $('#teacherContent').html('<div class="card">'+
           '<div class="card-body">'+
-            '<div class="row">'+
-              '<div class="col-12"><label>Nombre: '+response.name+'</label></div>'+
-              '<div class="col-12"><label>Apellido: '+response.lastname+'</label></div>'+
-              '<div class="col-12"><label>E-mail: '+response.email+'</label></div>'+
-            '</div>'+
+          '<div class="row">'+
+          '<div class="col-12"><label>Nombre: '+response.name+'</label></div>'+
+          '<div class="col-12"><label>Apellido: '+response.lastname+'</label></div>'+
+          '<div class="col-12"><label>E-mail: '+response.email+'</label></div>'+
           '</div>'+
-        '</div>');
+          '</div>'+
+          '</div>');
 
       }
     });
   });
-
-
-
-
-
-
 </script>
 @endsection
 @section('content')
 <div class="row">
+
   {{-- {{ $matter }} --}}
-  @foreach($matter_user as $items)
+
+  @forelse($matter_user as $items)
   @if($items->matter->career_id==$career->id)
-   <div class="col-xl-4 order-xl-2 mb-8 mb-xl-6">
-          <div class="card card-profile shadow">
-            <div class="row justify-content-center">
-              <div class="col-lg-6 order-lg-2">
-                <div class="card-profile-image">
-                  <a href="#">
-                    <img src="{{ asset('template/assets/img/theme/user.jpg') }}" class="rounded-circle">
-                  </a>
-                </div>
-              </div>
-            </div><br><br><br>
-            <div class="card-body pt-0 pt-md-5">
-              <div class="row">
-                <div class="col-6">
-                  <div class="card-profile-stats d-flex justify-content-center mt-md-5">
-                    <br>
-                  </div>
-                </div>
-              </div>
-
-              <div class="text-center">
-                <h3>
-                  {{ $items->user->name }} {{ $items->user->lastname }}
-                </h3>
-                <h4>
-                  CI: {{ $items->user->dni }}
-                </h4>
-                <div class="h5 font-weight-300">
-                  <i class="ni location_pin mr-2"></i>{{$items->type!=0?'Profesor':'Coordinador'}}
-                </div>
-                <div>
-                  <i class="ni education_hat mr-2"></i>Unidad Curricular
-                </div>
-                <div>
-                  {{ $items->matter->matter }}
-                </div>
-              </div>
-             
-
-             
+  <div class="col-xl-4 order-xl-2 mb-8 mb-xl-6">
+    <div class="card card-profile shadow">
+      <div class="row justify-content-center">
+        <div class="col-lg-6 order-lg-2">
+          <div class="card-profile-image">
+            <a href="#">
+              <img src="{{ asset('template/assets/img/theme/user.jpg') }}" class="rounded-circle">
+            </a>
+          </div>
+        </div>
+      </div><br><br><br>
+      <div class="card-body pt-0 pt-md-5">
+        <div class="row">
+          <div class="col-6">
+            <div class="card-profile-stats d-flex justify-content-center mt-md-5">
+              <br>
             </div>
           </div>
         </div>
+        <div class="text-center">
+          <h3>
+            {{ $items->user->name }} {{ $items->user->lastname }}
+          </h3>
+          <h4>
+            CI: {{ $items->user->dni }}
+          </h4>
+          <div class="h5 font-weight-300">
+            <i class="ni location_pin mr-2"></i>{{$items->type!=0?'Profesor':'Coordinador'}}
+          </div>
+          <div>
+            <i class="ni education_hat mr-2"></i>Unidad Curricular
+          </div>
+          <div>
+            {{ $items->matter->matter }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   @endif
-  @endforeach
+  @empty
+  <div class="alert alert-success col text-center">
+    <button type="button" class="close" data-dismiss="alert">&times;</button>
+      No hay Profesores para esta carrera
+  </div>
+  @endforelse 
 </div>
 
 @endsection
