53791321a4e37afbb8a230dd23b347973cf64309
root <jesusesteban12321@gmail.com>
2020-02-04 02:12:52 -0400

    boton para el modal de descargar contenidos por search de fecha

--- resources/views/layouts/sidebar/contentStudent.blade.php
+++ resources/views/layouts/sidebar/contentStudent.blade.php
@@ -3,3 +3,8 @@
     <i class="ni ni-cloud-download-95 text-info"></i> Descargar Eqivalencia
   </a>
 </li> 
+<li class="nav-item">
+  <a class="nav-link font-italic" href="#" data-target='#searchContent' data-toggle='modal'>
+    <i class="ni ni-cloud-download-95 text-success"></i> Descargar Contenido Por Fecha
+  </a>
+</li> 
