9effb638ea266327e3a40ae32b9af7acf2a228b4
root <jesusesteban12321@gmail.com>
2020-02-08 21:40:46 -0400

    template public para los pdf

---
+++ resources/views/pdf/layouts/appPublic.blade.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">	
+	<title>@yield('title')</title>
+	<meta name="generator" content="LibreOffice 5.4.0.3 (Linux)">
+	<meta name="created" content="2019-08-07T20:14:11">
+	<meta name="changed" content="2019-08-07T20:14:11">
+	<meta name="AppVersion" content="12.0000">
+	<meta name="DocSecurity" content="0">
+	<meta name="HyperlinksChanged" content="false">
+	<meta name="LinksUpToDate" content="false">
+	<meta name="ScaleCrop" content="false">
+	<meta name="ShareDoc" content="false">
+	@yield('style')
+	<style type="text/css">
+		body,div,table,thead,tbody,tfoot,tr,th,td,p { font-family:"Times New Roman"; font-size:x-small }
+		a.comment-indicator:hover + comment { background:#ffd; position:absolute; display:block; border:1px solid black; padding:0.5em;  } 
+		a.comment-indicator { background:red; display:inline-block; border:1px solid black; width:0.5em; height:0.5em;  } 
+		.comment{ display:none;  }
+		table {
+			width: 80% !important;
+			margin-left: 1cm !important;
+			margin-right: 3cm !important;
+			margin-top: 2cm !important;
+			margin-bottom: 2cm !important;
+			/*margin-top: 5%;*/
+			/*min-height: 96% !important;*/
+		} 
+		
+		td{
+			/*darle uniformidad a los bordes tablas*/
+			border:  1px solid black!important;
+		}
+
+		.reporte-campo-texto {
+			word-break: break-all !important;
+			text-align: left !important;
+			font: 11px monospace !important;
+		} 		
+
+		.columna-invisible {
+			border-color: transparent !important;
+		}
+
+		.contenido-invisible {
+			color: transparent !important;
+		}
+
+		.titulo-header-reporte {
+			font-size: 11px;
+		}
+
+		
+
+	@page {
+        margin: 0cm .5cm;
+        font-family: Arial;
+    }
+    body {
+        margin: 2cm 3cm 2cm 1cm;
+    }
+
+    header {
+		font: 25px monospace !important;
+        position: fixed;
+        top: 0cm;
+        left: 0cm;
+        right: 0cm;
+        height: auto;
+        line-height: 30px;
+		margin-bottom: 15px !important;
+
+    }
+    footer {
+        position: fixed;
+        bottom: 0cm;
+        left: 0cm;
+        right: 0cm;
+        height: 2cm;
+        line-height: 35px;
+		font:15px monospace !important;
+    }
+
+	th, td{
+		border:2px solid black !important;
+	}
+	.p{
+		font: 21px monospace !important;
+    	background: #ffffff;
+    	border: none; padding: 0in;
+    	page-break-inside: auto;
+    	widows: 0;
+    	orphans: 0;
+		background: transparent;
+    	page-break-after: auto;
+		text-transform: uppercase;
+ 
+    }.table {
+		width: 150% !important;
+		max-height: 60% !important;
+		max-width:150% !important;
+		height: 60% !important;
+		margin-left: 2cm !important;
+		margin-right: 6cm !important;
+		margin-top: 1cm !important;
+		margin-bottom: .5cm !important;
+		position: all ;
+	}
+	.td,.th{
+		border:  1px solid black !important;
+		outline: 1px solid black !important;
+		width:.5cm !important;
+		height:.4cm !important;
+		min-height: 10px !important;
+		padding: 1px;
+		margin:1px;
+		background: #ffffff !important;
+	}h4{
+		text-transform: uppercase;
+		font: 21px monospace !important;
+		font-weight: bold;
+	}h3{
+		text-transform: uppercase;
+		font: 19px monospace !important;
+		font-weight: bold;
+	}
+	.font{
+		word-break: break-all !important;
+		background: transparent;
+		color:#000000 !important;
+		text-align: left !important;
+		font: 20px monospace !important;
+		padding: 10px;
+	}pre{
+		word-break: keep-all !important;
+		background: transparent;
+		color:#000000 !important;
+		text-align: left !important;
+		font: 20px monospace !important;
+		
+	}
+	span{
+		color:#000000 !important;
+		background: #ffffff !important;
+		font-variant: normal;
+		text-decoration: none;
+		font-style: normal
+
+	}
+	
+	@media print {
+	  .nueva-pagina {
+	    	page-break-before: always;
+	  }
+	  .paginar {
+	    	page-break-before: always;
+	  }
+	}
+
+	.nueva-pagina {
+	    page-break-before: always;
+		margin-top: 8cm !important;
+		position: absolute !important;
+	}
+	</style>
+	@yield('style_inline')
+</head>
+<body>
+	@yield('content')
+</body>
+</html>
