4558705091de5d1115809ea445e92f1b69d9984b
root <jesusesteban12321@gmail.com>
2020-02-08 20:29:20 -0400

    linea de mas

--- resources/views/pdf/layouts/app.blade.php
+++ resources/views/pdf/layouts/app.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
 <head>
-	
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">	
 	<title>@yield('title')</title>
 	<meta name="generator" content="LibreOffice 5.4.0.3 (Linux)">
