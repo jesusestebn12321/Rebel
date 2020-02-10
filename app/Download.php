33126a3912910f13071e005bc1efde4c486a7b79
root <jesusesteban12321@gmail.com>
2020-02-09 23:32:59 -0400

    se le agrega en campo user_agent

--- app/Download.php
+++ app/Download.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Download extends Model
 {
     protected $table='downloads';
-    protected $fillable = ['id','slug','user_id','last_student','start_student', 'status'];
+    protected $fillable = ['id','slug','user_id','last_student','start_student', 'status','user_agent'];
 
     public function user(){
     	return $this->belongsTo(User::class,'user_id');
